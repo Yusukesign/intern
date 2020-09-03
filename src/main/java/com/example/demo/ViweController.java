c5724d0c3ecd8a2b0033725ed70894d5a1f8fd3e
Yusukesign <yusuke07301106@icloud.com>
2020-09-03 15:55:50 +0000

    実装

---
+++ src/main/java/com/example/demo/ViweController.java
@@ -0,0 +1,14 @@
+public package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class ViweController {
+    @GetMapping
+    public String View(){
+        return "view";
+    }
+}
